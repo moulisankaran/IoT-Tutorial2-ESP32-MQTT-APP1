1e58e02b8cc4fd47c39b2fc3605945a7973301f0
Mouli Sankaran <mouli.sankaran@yahoo.com>
2021-07-09 18:39:20 +0530

    CA certificate in the char * string format

---
+++ ESP32-code/MQTT_TLS_ESP32_SNS2/MyMQTTBrokerCA.h
@@ -0,0 +1,39 @@
+//**********************************************************************************************************
+// Author: Mouli Sankaran (mouli.sankaran@yahoo.com)
+// Date: 7 Jul 2021
+// This file has the Certificate Authority (CA) certificate from the HiveMQ for this particular 
+// Instance of MQTT Broker running on the cloud. This CA is required to encrypt the data being
+// Shared between the MQTT Broker and the IoT device as part of the TLS protocol to have secure connection.
+//**********************************************************************************************************
+
+#ifndef MYMQTTBROKERCA_H
+#define MYMQTTBROKERCA_H
+
+// Changed the below cerfiticate contents with the CA downloaded from HiveMQ
+// To make the contents of the CA as Char * to be fed to the Client code to
+// This is used to encrypt the data before sending it over the TLS connection to the MQTT Broker running on the Cloud
+// Make sure that you replace the below contents with the CA file that you download from the HiveMQ for your MQTT broker instance.
+
+const char* ca_cert = \ 
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/\n" \
+"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \
+"DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow\n" \
+"PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD\n" \
+"Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\n" \
+"AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O\n" \
+"rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq\n" \
+"OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b\n" \
+"xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw\n" \
+"7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD\n" \
+"aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV\n" \
+"HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG\n" \
+"SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69\n" \
+"ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr\n" \
+"AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz\n" \
+"R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5\n" \
+"JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo\n" \
+"Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\n" \
+"-----END CERTIFICATE-----\n";
+
+#endif //MYMQTTBROKERCA_H
